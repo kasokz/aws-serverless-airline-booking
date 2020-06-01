d48e5554df3365d51bc1dd4360da89f3ebb37950
Simon <simon.eismann@uni-wuerzburg.de>
2020-06-01 13:57:12 +0200

    reworked for multikey structure

--- src/backend/api-lambda-stripe-charge/charge.js
+++ src/backend/api-lambda-stripe-charge/charge.js
@@ -3,8 +3,7 @@ const AWS = require('aws-sdk'),
   qs = require('querystring'),
   processResponse = require('./src/process-response'),
   createCharge = require('./src/create-charge'),
-  STRIPE_SECRET_KEY_NAME1 = `/${process.env.SSM_PARAMETER_PATH1}`,
-  STRIPE_SECRET_KEY_NAME2 = `/${process.env.SSM_PARAMETER_PATH2}`,
+  STRIPE_SECRET_KEY_NAME = `/${process.env.SSM_PARAMETER_PATH}`
   IS_CORS = true;
 
 exports.handler = (event) => {
@@ -19,29 +18,19 @@ exports.handler = (event) => {
   if (!chargeRequest.amount || !chargeRequest.currency) {
     return Promise.resolve(processResponse(IS_CORS, 'invalid arguments, please provide amount and currency fields as mentioned in the app README', 400));
   }
-
-  cas = Math.floor(Math.random() * 2)
-  if (cas == 0) {
-	  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME1, WithDecryption: true }).promise()
-		.then(response => {
-		  const stripeSecretKeyValue = response.Parameter.Value;
-		  return createCharge(stripeSecretKeyValue, chargeRequest.stripeToken, chargeRequest.email, chargeRequest.amount, chargeRequest.currency, chargeRequest.description);
-		})
-		.then(createdCharge => processResponse(IS_CORS, { createdCharge }))
-		.catch((err) => {
-		  console.log(err);
-		  return processResponse(IS_CORS, { err }, 500);
-		});
-  } else {
-	  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME2, WithDecryption: true }).promise()
-		.then(response => {
-		  const stripeSecretKeyValue = response.Parameter.Value;
-		  return createCharge(stripeSecretKeyValue, chargeRequest.stripeToken, chargeRequest.email, chargeRequest.amount, chargeRequest.currency, chargeRequest.description);
-		})
-		.then(createdCharge => processResponse(IS_CORS, { createdCharge }))
-		.catch((err) => {
-		  console.log(err);
-		  return processResponse(IS_CORS, { err }, 500);
-		});
-  }
+  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME1, WithDecryption: true }).promise()
+	.then(response => {
+	  const stripeSecretKeys = response.Parameter.Value.split(',');
+	  const keyId = 0;
+	  if(typeof(chargeRequest.stripeKey) !== "undefined"){
+          keyId = chargeRequest.stripeKey
+      }
+	  const stripeSecretKeyValue = stripeSecretKeys[keyId]
+	  return createCharge(stripeSecretKeyValue, chargeRequest.stripeToken, chargeRequest.email, chargeRequest.amount, chargeRequest.currency, chargeRequest.description);
+	})
+	.then(createdCharge => processResponse(IS_CORS, { createdCharge }))
+	.catch((err) => {
+	  console.log(err);
+	  return processResponse(IS_CORS, { err }, 500);
+	});
 };
