2dfd9e011739c1b3215f07d50a13523872bd70fd
Simon <simon.eismann@uni-wuerzburg.de>
2020-06-01 16:50:29 +0200

    fixed typo

--- src/backend/api-lambda-stripe-charge/charge.js
+++ src/backend/api-lambda-stripe-charge/charge.js
@@ -18,7 +18,7 @@ exports.handler = (event) => {
   if (!chargeRequest.amount || !chargeRequest.currency) {
     return Promise.resolve(processResponse(IS_CORS, 'invalid arguments, please provide amount and currency fields as mentioned in the app README', 400));
   }
-  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME1, WithDecryption: true }).promise()
+  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME, WithDecryption: true }).promise()
 	.then(response => {
 	  const stripeSecretKeys = response.Parameter.Value.split(',');
 	  const keyId = 0;
