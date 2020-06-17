2eb12d45a3cd8207ee929b69feb8cb8cfc9fd925
Simon <simon.eismann@uni-wuerzburg.de>
2020-06-17 12:41:24 +0200

    added api version and httpagent

--- src/backend/api-lambda-stripe-charge/src/payment-processor.js
+++ src/backend/api-lambda-stripe-charge/src/payment-processor.js
@@ -8,7 +8,9 @@ const { captureAsyncFunc } = require('./tracing-repository'),
 module.exports = {
   createCharge: async function (stripeSecretKey, token, amount, currency, isCapture, description = 'Charge Description'){
     const stripe = require('stripe')(stripeSecretKey, {
+  apiVersion: '2019-08-08',
   maxNetworkRetries: 2,
+  httpAgent: new ProxyAgent(process.env.http_proxy),
   timeout: 2000,
   host: 'api.example.com',
   port: 123,
@@ -26,22 +28,27 @@ module.exports = {
     );
   },
   captureCharge: async function (stripeSecretKey, charge){
-    const stripe = require('stripe')(stripeSecretKey, {
+  apiVersion: '2019-08-08',
   maxNetworkRetries: 2,
+  httpAgent: new ProxyAgent(process.env.http_proxy),
   timeout: 2000,
   host: 'api.example.com',
   port: 123,
   telemetry: false,
 });
+});
     return await captureAsyncFunc(CAPTURE_CHARGE_MESSAGE_TRACE, () => stripe.charges.capture(charge));
   },
   createRefund: async function (stripeSecretKey, charge) {
     const stripe = require('stripe')(stripeSecretKey, {
+  apiVersion: '2019-08-08',
   maxNetworkRetries: 2,
+  httpAgent: new ProxyAgent(process.env.http_proxy),
   timeout: 2000,
   host: 'api.example.com',
   port: 123,
   telemetry: false,
+});
 });
     return await captureAsyncFunc(CREATE_REFUND_MESSAGE_TRACE, () => stripe.refunds.create({charge}));
   }
