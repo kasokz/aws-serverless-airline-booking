6ac711930b522cd13c4c719801f4f65a3797652c
Simon <simon.eismann@uni-wuerzburg.de>
2020-06-18 17:22:28 +0200

    versioning

--- src/backend/api-lambda-stripe-charge/src/payment-processor.js
+++ src/backend/api-lambda-stripe-charge/src/payment-processor.js
@@ -7,9 +7,7 @@ const { captureAsyncFunc } = require('./tracing-repository'),
 
 module.exports = {
   createCharge: async function (stripeSecretKey, token, amount, currency, isCapture, description = 'Charge Description'){
-	const stripe = require('stripe')(stripeSecretKey, {
-  apiVersion: '2020-03-02',
-});
+	const stripe = require('stripe')(stripeSecretKey);
 
     return await captureAsyncFunc(CREATE_CHARGE_MESSAGE_TRACE, () => 
       stripe.charges.create({
@@ -22,15 +20,11 @@ module.exports = {
     );
   },
   captureCharge: async function (stripeSecretKey, charge){
-	const stripe = require('stripe')(stripeSecretKey, {
-  apiVersion: '2020-03-02',
-});
+	const stripe = require('stripe')(stripeSecretKey);
     return await captureAsyncFunc(CAPTURE_CHARGE_MESSAGE_TRACE, () => stripe.charges.capture(charge));
   },
   createRefund: async function (stripeSecretKey, charge) {
-	const stripe = require('stripe')(stripeSecretKey, {
-  apiVersion: '2020-03-02',
-});
+	const stripe = require('stripe')(stripeSecretKey);
     return await captureAsyncFunc(CREATE_REFUND_MESSAGE_TRACE, () => stripe.refunds.create({charge}));
   }
 };
